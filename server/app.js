9813865a573e7c49bf4de345fcbe06fafbe494db
Himani Mense <46218508+Himani95@users.noreply.github.com>
2020-07-05 17:44:07 -0700

    Add files via upload
    To Demonstrate the utility of Node/Express server

---
+++ server/app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors=require("cors");//Declared variable t and import the cross origin resource sharing package
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+testAPIRouter= require("./routes/testAPI");//Declared variable for using testAPI which is passed below on line 29
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());//To use the particular API
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+/*Date: 4 July 2020
+Author: Himani
+Description: To demonstrate the utilization of the Node/Express server(Server Side Rendering)
+We have imported the testAPI file so that it can be used*/
+app.use("/testAPI", testAPIRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
